8eed420543199fd61b1c4baaf93c83427ca65281
hercule-fabien <vvg.doc@yandex.ru>
2022-10-06 20:23:26 +0300

    Added menu to main.py and created bank_account.py

---
+++ bank_account.py
@@ -0,0 +1,50 @@
+account = {
+    "account_value": 0,
+    "purchase_history": {},
+}
+
+
+def account_refill():
+    amount = float(input("На какую сумму пополнить счет? "))
+    return amount
+
+
+def purchase(account_value):
+    purchase_sum = float(input("Введите сумму покупки: "))
+    if purchase_sum > account_value:
+        print("*" * 10)
+        print("Не достаточно средств, пополните счет.")
+        print("*" * 10)
+    else:
+        purchase_item = input("Что вы хотите купить? ")
+        money_left = account_value - purchase_sum
+        account["purchase_history"][purchase_item] = purchase_sum
+        account['account_value'] = money_left
+        print(f"Вы купили {purchase_item}")
+        print(f"На счету осталось {account['account_value']} денег")
+
+
+def purchase_history():
+    for key, value in account["purchase_history"].items():
+        print(f"Вы купили {key} на сумму {value}")
+
+
+while True:
+    print('1. пополнение счета')
+    print('2. покупка')
+    print('3. история покупок')
+    print('4. выход')
+
+    choice = input('Выберите пункт меню: ')
+    if choice == '1':
+        account['account_value'] = account_refill()
+        print(
+            f"Счет успешно пополнен. На вашем счету {account['account_value']} денег")
+    elif choice == '2':
+        purchase(account['account_value'])
+    elif choice == '3':
+        purchase_history()
+    elif choice == '4':
+        break
+    else:
+        print('Неверный пункт меню')
---
+++ main.py
@@ -0,0 +1,22 @@
+def main():
+    print("Welcome to the File Manager")
+    print("*" * 27)
+    print("Please pick your desired action from the menu below: ")
+    print("""
+        1. Create a folder.
+        2. Delete (file/folder).
+        3. Copy (file/folder).
+        4. Check the files in the working directory.
+        5. Display folders only.
+        6. Display files only.
+        7. Check OS type.
+        8. Credits.
+        9. Play the Victory game.
+        10. Check you bank account status.
+        11. Change working directory.
+        12. Quit.
+    """)
+
+
+if __name__ == '__main__':
+    main()
